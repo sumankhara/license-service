bd9976703b87b0fa867c4447592596694ee34ebc
Suman Khara <suman.khara@cerner.com>
2020-12-16 10:30:00 +0530

    format

--- src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -1,17 +1,5 @@
 package com.optimagrowth.license.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
-import java.util.concurrent.TimeoutException;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.stereotype.Service;
-
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.model.Organization;
 import com.optimagrowth.license.repository.LicenseRepository;
@@ -19,131 +7,137 @@ import com.optimagrowth.license.service.client.OrganizationDiscoveryClient;
 import com.optimagrowth.license.service.client.OrganizationFeignClient;
 import com.optimagrowth.license.service.client.OrganizationRestTemplateClient;
 import com.optimagrowth.license.utils.UserContextHolder;
-
-import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.stereotype.Service;
 
 @Service
 public class LicenseService {
-	
-	@Autowired
-	private MessageSource messageSource;
-	
-	@Autowired
-	private LicenseRepository licenseRepository;
-	
-	@Autowired
-	private OrganizationDiscoveryClient organizationDiscoveryClient;
-	
-	@Autowired
-	private OrganizationRestTemplateClient organizationRestClient;
-	
-	@Autowired
-	private OrganizationFeignClient organizationFeignClient;
-	
-	private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
 
-	public License getLicense(String licenseId, String organizationId, String clientType) {
-		License license = licenseRepository.findByLicenseId(licenseId);
-		if(null == license) {
-			throw new IllegalArgumentException(String.format(messageSource.getMessage("license.search.error.message", null, null), licenseId, organizationId));
-		}
-		
-		Organization organization = retrieveOrganizationInfo(organizationId, clientType);
-		if (null != organization) {
-			license.setOrganizationName(organization.getName());
-			license.setContactName(organization.getContactName());
-			license.setContactEmail(organization.getContactEmail());
-			license.setContactPhone(organization.getContactPhone());
-		}
-		return license;
-	}
-	
-	private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
-		Organization organization = null;
+  @Autowired private MessageSource messageSource;
 
-		switch (clientType) {
-		case "feign":
-			System.out.println("I am using the feign client");
-			organization = organizationFeignClient.getOrganization(organizationId);
-			break;
-		case "rest":
-			System.out.println("I am using the rest client");
-			organization = organizationRestClient.getOrganization(organizationId);
-			break;
-		case "discovery":
-			System.out.println("I am using the discovery client");
-			organization = organizationDiscoveryClient.getOrganization(organizationId);
-			break;
-		default:
-			organization = organizationRestClient.getOrganization(organizationId);
-			break;
-		}
+  @Autowired private LicenseRepository licenseRepository;
 
-		return organization;
-	}
+  @Autowired private OrganizationDiscoveryClient organizationDiscoveryClient;
 
-	public License createLicense(License license) {
-		license.setLicenseId(UUID.randomUUID().toString());
-		licenseRepository.save(license);
-		
-		return license;
-	}
+  @Autowired private OrganizationRestTemplateClient organizationRestClient;
 
-	public License updateLicense(License license) {
-		licenseRepository.save(license);
-		
-		return license;
-	}
+  @Autowired private OrganizationFeignClient organizationFeignClient;
 
-	public String deleteLicense(String licenseId) {
-		String responseMessage = null;
-		License license = new License();
-		license.setLicenseId(licenseId);
-		licenseRepository.delete(license);
-		responseMessage = String.format("Deleting license with id %s", licenseId);
-		
-		return responseMessage;
-	}
-	
-	@CircuitBreaker(name = "licenseService", fallbackMethod = "buildFallbackLicenseList")
-	//@Bulkhead(name = "bulkheadLicenseService", type = Bulkhead.Type.THREADPOOL, fallbackMethod = "buildFallbackLicenseList")
-	//@Retry(name = "retryLicenseService", fallbackMethod = "buildFallbackLicenseList")
-	//@RateLimiter(name = "licenseService", fallbackMethod = "buildFallbackLicenseList")
-	public List<License> getLicensesByOrganization(String organizationId) throws TimeoutException {
-		logger.debug("LicenseService:getLicensesByOrganization Correlation id: {}",
-				UserContextHolder.getContext().getCorrelationId());
-		randomlyRunLong();
-		return licenseRepository.findByOrganizationId(organizationId);
-	}
-	
-	private List<License> buildFallbackLicenseList(String organizationId, Throwable t) {
-		List<License> fallbackList = new ArrayList<>();
-		License license = new License();
-		license.setLicenseId("0000000-00-00000");
-		license.setOrganizationId(organizationId);
-		license.setProductName("Sorry no licensing information currently available");
-		fallbackList.add(license);
-		return fallbackList;
-	}
-	
-	private void randomlyRunLong() throws TimeoutException {
-		Random rand = new Random();
-		int randomNum = rand.nextInt((3 - 1) + 1) + 1;
-		if(randomNum == 3) {
-			sleep();
-		}
-	}
-	
-	private void sleep() throws TimeoutException {
-		try {
-			Thread.sleep(5000);
-			throw new TimeoutException();
-		}
-		catch(InterruptedException ex) {
-			ex.printStackTrace();
-		}
-	}
+  private static final Logger logger = LoggerFactory.getLogger(LicenseService.class);
+
+  public License getLicense(String licenseId, String organizationId, String clientType) {
+    License license = licenseRepository.findByLicenseId(licenseId);
+    if (null == license) {
+      throw new IllegalArgumentException(
+          String.format(
+              messageSource.getMessage("license.search.error.message", null, null),
+              licenseId,
+              organizationId));
+    }
+
+    Organization organization = retrieveOrganizationInfo(organizationId, clientType);
+    if (null != organization) {
+      license.setOrganizationName(organization.getName());
+      license.setContactName(organization.getContactName());
+      license.setContactEmail(organization.getContactEmail());
+      license.setContactPhone(organization.getContactPhone());
+    }
+    return license;
+  }
+
+  private Organization retrieveOrganizationInfo(String organizationId, String clientType) {
+    Organization organization = null;
+
+    switch (clientType) {
+      case "feign":
+        System.out.println("I am using the feign client");
+        organization = organizationFeignClient.getOrganization(organizationId);
+        break;
+      case "rest":
+        System.out.println("I am using the rest client");
+        organization = organizationRestClient.getOrganization(organizationId);
+        break;
+      case "discovery":
+        System.out.println("I am using the discovery client");
+        organization = organizationDiscoveryClient.getOrganization(organizationId);
+        break;
+      default:
+        organization = organizationRestClient.getOrganization(organizationId);
+        break;
+    }
+
+    return organization;
+  }
+
+  public License createLicense(License license) {
+    license.setLicenseId(UUID.randomUUID().toString());
+    licenseRepository.save(license);
+
+    return license;
+  }
+
+  public License updateLicense(License license) {
+    licenseRepository.save(license);
+
+    return license;
+  }
+
+  public String deleteLicense(String licenseId) {
+    String responseMessage = null;
+    License license = new License();
+    license.setLicenseId(licenseId);
+    licenseRepository.delete(license);
+    responseMessage = String.format("Deleting license with id %s", licenseId);
+
+    return responseMessage;
+  }
+
+  @CircuitBreaker(name = "licenseService", fallbackMethod = "buildFallbackLicenseList")
+  // @Bulkhead(name = "bulkheadLicenseService", type = Bulkhead.Type.THREADPOOL, fallbackMethod =
+  // "buildFallbackLicenseList")
+  // @Retry(name = "retryLicenseService", fallbackMethod = "buildFallbackLicenseList")
+  // @RateLimiter(name = "licenseService", fallbackMethod = "buildFallbackLicenseList")
+  public List<License> getLicensesByOrganization(String organizationId) throws TimeoutException {
+    logger.debug(
+        "LicenseService:getLicensesByOrganization Correlation id: {}",
+        UserContextHolder.getContext().getCorrelationId());
+    randomlyRunLong();
+    return licenseRepository.findByOrganizationId(organizationId);
+  }
+
+  private List<License> buildFallbackLicenseList(String organizationId, Throwable t) {
+    List<License> fallbackList = new ArrayList<>();
+    License license = new License();
+    license.setLicenseId("0000000-00-00000");
+    license.setOrganizationId(organizationId);
+    license.setProductName("Sorry no licensing information currently available");
+    fallbackList.add(license);
+    return fallbackList;
+  }
+
+  private void randomlyRunLong() throws TimeoutException {
+    Random rand = new Random();
+    int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+    if (randomNum == 3) {
+      sleep();
+    }
+  }
+
+  private void sleep() throws TimeoutException {
+    try {
+      Thread.sleep(5000);
+      throw new TimeoutException();
+    } catch (InterruptedException ex) {
+      ex.printStackTrace();
+    }
+  }
 }
--- src/main/java/com/optimagrowth/license/utils/UserContextFilter.java
+++ src/main/java/com/optimagrowth/license/utils/UserContextFilter.java
@@ -1,7 +1,6 @@
 package com.optimagrowth.license.utils;
 
 import java.io.IOException;
-
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -9,35 +8,38 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserContextFilter implements Filter {
-	
-	private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
-			throws IOException, ServletException {
-		
-		HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-		
-		UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
-		UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
-		UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
-		UserContextHolder.getContext().setOrganizationId(httpServletRequest.getHeader(UserContext.ORGANIZATION_ID));
-		
-		logger.debug("UserContextFilter Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
-		
-		filterChain.doFilter(httpServletRequest, response);
-	}
+  private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
 
-	@Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
+      throws IOException, ServletException {
 
-    @Override
-    public void destroy() {}
+    HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+
+    UserContextHolder.getContext()
+        .setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
+    UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+    UserContextHolder.getContext()
+        .setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+    UserContextHolder.getContext()
+        .setOrganizationId(httpServletRequest.getHeader(UserContext.ORGANIZATION_ID));
+
+    logger.debug(
+        "UserContextFilter Correlation id: {}", UserContextHolder.getContext().getCorrelationId());
+
+    filterChain.doFilter(httpServletRequest, response);
+  }
+
+  @Override
+  public void init(FilterConfig filterConfig) throws ServletException {}
+
+  @Override
+  public void destroy() {}
 }
