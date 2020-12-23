bb3791ec265bf6ef42b790ff66cc020af5c69d55
Suman Khara <suman.khara@cerner.com>
2020-12-23 14:52:53 +0530

    ch08

--- src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
+++ src/main/java/com/optimagrowth/license/LicenseServiceApplication.java
@@ -1,5 +1,7 @@
 package com.optimagrowth.license;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
@@ -10,9 +12,12 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import com.optimagrowth.license.utils.UserContextInterceptor;
 
 @SpringBootApplication
 @RefreshScope
@@ -42,6 +47,16 @@ public class LicenseServiceApplication {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
+		RestTemplate template = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+		if (interceptors==null){
+            template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+        }
+        else{
+            interceptors.add(new UserContextInterceptor());
+            template.setInterceptors(interceptors);
+        }
+
+        return template;
 	}
 }
